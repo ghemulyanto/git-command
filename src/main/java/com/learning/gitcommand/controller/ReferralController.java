835f78dfce1d01aaf4ad3a76dca29d71525cc9ce
ghemulyanto <trimulyanto.tm@gmail.com>
2022-05-20 09:53:52 +0700

    adding new feature referral

---
+++ src/main/java/com/learning/gitcommand/controller/ReferralController.java
@@ -0,0 +1,15 @@
+package com.learning.gitcommand.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/referral")
+public class ReferralController {
+
+  @GetMapping()
+  public String getReferral(){
+    return "referral";
+  }
+}
