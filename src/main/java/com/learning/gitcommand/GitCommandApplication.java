68a121511d7ae0f6eb06eb71e58712f3aaf28448
ghemulyanto <trimulyanto.tm@gmail.com>
2022-05-20 09:50:05 +0700

    initial commit

---
+++ src/main/java/com/learning/gitcommand/GitCommandApplication.java
@@ -0,0 +1,13 @@
+package com.learning.gitcommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/gitcommand/GitCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.gitcommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
